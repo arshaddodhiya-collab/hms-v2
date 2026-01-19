54dc9a284e2c1b4a8250e5e209984a2a38d9f9c5
arshaddodhiya-collab <arshad.dodhiya@artemhealthtech.com>
2026-01-19 11:09:33 +0530

    patient registration feature setup

---
+++ src/app/features/patient-reg/patient-reg.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PatientRegRoutingModule } from './patient-reg-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PatientRegRoutingModule
+  ]
+})
+export class PatientRegModule { }
